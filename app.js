dfb3f58f468598aa43101041cdc086d10794762d
Nawlesh034 <nn802301@gmail.com>
2025-08-22 12:29:28 +0530

    cors-changes

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(cors({
-     origin: "https://ecomm-opal-psi.vercel.app",
+  origin: ["https://ecomm-opal-psi.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 app.use(express.json());
+app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
